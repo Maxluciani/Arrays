fd0c732e94a758be295ef3738ee7b8df22d2fdaf
Entra21 <entra21@proway.treina>
2020-09-29 18:48:31 -0300

    segunda lista de arrays

--- lista2.cs
+++ lista2.cs
@@ -10,7 +10,31 @@ namespace Arrays
         }
        static void ArrayLista2_01()
        {
-           System.Console.WriteLine("Hello word");
+           // Popule dois vetores com 10 valores cada. Após esta operação, troque o conteúdo dos
+          // vetores.
+          const int  tamanhoarray = 3;
+          double[] a = new double[tamanhoarray];
+          double [] b = new double[tamanhoarray];
+          double[] invert = new double[tamanhoarray];
+          for (int i = 0; i < tamanhoarray; i++)
+          {
+              System.Console.WriteLine($"Digite o {i+1}° numero da lista");
+              a[i] = double.Parse(Console.ReadLine());
+              invert[i] = a[i];
+          }
+              for (int i = 0; i < tamanhoarray; i++)
+              {
+                 System.Console.WriteLine($"Digite o {i+1}º numero da segunda lista"); 
+                 b[i] = double.Parse(Console.ReadLine());
+              a[i] = b[i];
+              }
+
+              for (int i = 0; i < tamanhoarray; i++)
+              {
+                  b[i] = invert[i];
+              }
+               System.Console.WriteLine(String.Join(",",a));
+               System.Console.WriteLine(String.Join(",",b));
        }
 
 
